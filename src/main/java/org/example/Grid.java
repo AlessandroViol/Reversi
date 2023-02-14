29127176fc393caafa8dbf0520eb321f74f92fd6
F1397 <federica.azzalini@gmail.com>
2023-02-14 12:07:14 +0100

    Add Default Grid Test

---
+++ src/main/java/org/example/Grid.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class Grid {
+
+    int[][] grid;
+}
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,2 +1,36 @@
-public class test {
+package org.example;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+
+class TestGrid {
+
+    @Test
+    void DefaultGrid(){
+        Grid grid = new Grid();
+
+            int[][] TestGrid = {{0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, -1, 1, 0, 0, 0},
+                    {0, 0, 0, 1, -1, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0}};
+
+
+            assertArrayEquals(TestGrid, grid.grid);
+
+
+
+
+
+
+        }
 }
+
+
