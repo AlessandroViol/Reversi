f59885c9e37e4ec8d586be5148d2661aebf4db1a
F1397 <federica.azzalini@gmail.com>
2023-02-14 12:12:49 +0100

    Grid Initialization

--- src/main/java/org/example/Grid.java
+++ src/main/java/org/example/Grid.java
@@ -1,6 +1,19 @@
 package org.example;
 
 public class Grid {
+    int size = 8;
+    int grid[][] = new int[size][size];
 
-    int[][] grid;
+    public Grid() {
+
+        grid[3][3] = -1;
+        grid[4][3] = 1;
+        grid[3][4] = 1;
+        grid[4][4] = -1;
+
+
+
+    }
+
+
 }
