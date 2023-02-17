d0232f423fb88a2be3baede89323058791f32f04
F1397 <federica.azzalini@gmail.com>
2023-02-17 11:41:40 +0100

    Add Player Class

---
+++ src/main/java/units/sdm/Player.java
@@ -0,0 +1,16 @@
+package units.sdm;
+
+public class Player {
+
+    String name;
+    int colour;
+
+    public Player(String name, int colour) {
+        this.name = name;
+        this.colour = colour;
+
+    }
+
+}
+
+
