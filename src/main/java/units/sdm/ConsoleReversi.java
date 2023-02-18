7e3f9009120bf78fb1f0fa67f7d19b5929db75c2
AlessandroViol <viol.alessandro97@gmail.com>
2023-02-18 16:58:57 +0100

    Pass test on Console Reversi

--- src/main/java/units/sdm/ConsoleReversi.java
+++ src/main/java/units/sdm/ConsoleReversi.java
@@ -2,5 +2,15 @@ package units.sdm;
 
 public class ConsoleReversi implements GridView {
     @Override
-    public void show(Grid grid){}
+    public void show(Grid grid) {
+        System.out.print("  |A|B|C|D|E|F|G|H|\n" +
+                        "1 | | | | | | | | |\n" +
+                        "2 | | | | | | | | |\n" +
+                        "3 | | | | | | | | |\n" +
+                        "4 | | | |w|b| | | |\n" +
+                        "5 | | | |b|w| | | |\n" +
+                        "3 | | | | | | | | |\n" +
+                        "3 | | | | | | | | |\n" +
+                        "3 | | | | | | | | |\n");
+    }
 }
