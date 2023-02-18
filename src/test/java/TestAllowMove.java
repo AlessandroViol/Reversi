d03ba439f55e77d067fff26241f46cbccc1157f5
marcolg26 <62360013+marcolg26@users.noreply.github.com>
2023-02-18 12:00:02 +0100

    Add Test Allow Move

---
+++ src/test/java/TestAllowMove.java
@@ -0,0 +1,31 @@
+package units.sdm;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestAllowMove {
+    @Test
+    void DefaultGrid(){
+
+        Grid grid = new Grid();
+
+        int[][] check = {{0, 0, 0, 0, 0, 0, 0, 0},
+                         {0, 0, 0, 0, 0, 0, 0, 0},
+                         {0, 0, 0, 2, 0, 0, 0, 0},
+                         {0, 0, 2, -1, 1, 0, 0, 0},
+                         {0, 0, 0, 1, -1, 2, 0, 0},
+                         {0, 0, 0, 0, 2, 0, 0, 0},
+                         {0, 0, 0, 0, 0, 0, 0, 0},
+                         {0, 0, 0, 0, 0, 0, 0, 0}};
+
+        for(int i=0; i<8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (grid.AllowMove(i, j, 1)) grid.grid[i][j] = 2;
+            }
+        }
+
+        assertArrayEquals(check, grid.grid);
+
+    }
+
+
+}
