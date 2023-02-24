64adc2c0109b27f9ddf2699f9a3a7ccab597a54f
F1397 <federica.azzalini@gmail.com>
2023-02-24 17:31:16 +0100

    Change Access Modifiers for Player Class

--- src/main/java/units/sdm/Player.java
+++ src/main/java/units/sdm/Player.java
@@ -2,15 +2,25 @@ package units.sdm;
 
 public class Player {
 
-    String name;
-    int colour;
+    private String name;
+    private int colour;
 
     public Player(String name, int colour) {
         this.name = name;
         this.colour = colour;
 
+
+    }
+    public String getName(){
+        return name;
     }
 
+    public void setName(String name){
+        this.name = name;
+    }
+    public int getColour(){
+        return colour;
+    }
 }
 
 
