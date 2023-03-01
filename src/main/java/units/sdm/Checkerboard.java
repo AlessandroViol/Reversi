35b96aa5738b5349c3f1431071cb77d93c2914e8
F1397 <124884555+F1397@users.noreply.github.com>
2023-03-01 12:17:19 +0100

    Add comments

--- src/main/java/units/sdm/Checkerboard.java
+++ src/main/java/units/sdm/Checkerboard.java
@@ -7,12 +7,14 @@ public class Checkerboard {
     private int numberOfWhites;
     private int numberOfBlacks;
 
+    //Values coding the values of the black and white pieces and empty and allowed places on the checkerboard
     public final static int B = 1;
     public final static int W = -1;
     public final static int N = 0;
     public final static int A = 2;
 
 
+    //zero argument constructor that initialize the default checkerboard for reversi
     public Checkerboard() {
         checkerboard = new int[SIZE][SIZE];
 
@@ -22,6 +24,7 @@ public class Checkerboard {
         checkerboard[4][4] = W;
     }
 
+    //one argument constructor that copies the matrix of a given checkerboard
     public Checkerboard(int[][] checkerboard) {
         this.checkerboard = new int[SIZE][SIZE];
         for (int i = 0; i < SIZE; i++)
@@ -29,6 +32,7 @@ public class Checkerboard {
                 this.checkerboard[i][j] = checkerboard[i][j];
     }
 
+    //attribute getters for class attributes
     public int[][] getCheckerboard() {
         return this.checkerboard;
     }
@@ -41,16 +45,24 @@ public class Checkerboard {
         return numberOfBlacks;
     }
 
+    //computes if a piece of the provided color can be placed in the specified square of the checkerboard
     public boolean allowPlace(int posX, int posY, int colorTurn) {
-        if (checkerboard[posX][posY] != N && checkerboard[posX][posY] != A) return false;
+        //check that the specified square is empty
+        if (checkerboard[posX][posY] != N && checkerboard[posX][posY] != A)
+            return false;
 
+        //look at the adjacent squares in different directions using vertical and horizontal offsets
         for (int offsetX = -1; offsetX < 2; offsetX++) {
+            //check that the vertical adjacent squares are in-bound
             if (posX + offsetX >= 0 && posX + offsetX < SIZE) {
                 for (int offsetY = -1; offsetY < 2; offsetY++) {
+                    //skip if looking at the same square
                     if (offsetX == 0 && offsetY == 0)
                         continue;
 
+                    //check that the horizontal adjacent squares are in bound
                     if (posY + offsetY >= 0 && posY + offsetY < SIZE) {
+                        //if the adjacent square has a disk of opposite color checks if there is a full line of disks ending with a disk of the same color
                         if (checkerboard[posX + offsetX][posY + offsetY] == -colorTurn) {
                             if (checkDirection(offsetX, offsetY, posX + offsetX, posY + offsetY, colorTurn)) {
                                 return true;
@@ -61,47 +73,60 @@ public class Checkerboard {
             }
         }
 
+        //if none of the adjacent squares lead to available lines return false
         return false;
 
     }
 
+    //check if in the specified direction there is a contiguous line of disks of the same color terminating with a disk of the opposite color
     private boolean checkDirection(int offsetX, int offsetY, int posX, int posY, int colourTurn) {
-        while (posX + offsetX >= 0 && posX + offsetX < SIZE && posY + offsetY >= 0 && posY + offsetY < SIZE) { //
-
-            if (checkerboard[posX + offsetX][posY + offsetY] == colourTurn) { //
+        //move along the squares in the specified direction while in-bound
+        while (posX + offsetX >= 0 && posX + offsetX < SIZE && posY + offsetY >= 0 && posY + offsetY < SIZE) {
+            //if a disk of the original colour is encountered then in this direction there is an allowed line, so return true
+            if (checkerboard[posX + offsetX][posY + offsetY] == colourTurn) {
                 return true;
-
-            } else if (checkerboard[posX + offsetX][posY + offsetY] == N || checkerboard[posX + offsetX][posY + offsetY] == A) {
-                return false;
-
             }
+            //if an empty or allowed square is encountered then in this direction there isn't an allowed line, so return false
+            else if (checkerboard[posX + offsetX][posY + offsetY] == N || checkerboard[posX + offsetX][posY + offsetY] == A) {
+                return false;
+            }
+            //look at the next square
             posX = posX + offsetX;
             posY = posY + offsetY;
-
         }
+        //when the border is reached it means this is not an allowed direction and return false
         return false;
     }
 
+    //if the specified placement is allowed then place the disk and update the checkerboard
     protected boolean place(int posX, int posY, int colourTurn) {
 
         if (allowPlace(posX, posY, colourTurn)) {
             checkerboard[posX][posY] = colourTurn;
             updateCheckerboard(posX, posY, colourTurn);
-        } else return false;
+        }
+        else
+            return false;
 
         return true;
     }
 
+    //after a placement swap the color of all the adjacent disks of all the allowed lines
     protected void updateCheckerboard(int posX, int posY, int colourTurn) {
-
+        //look at the adjacent squares in different directions using vertical and horizontal offsets
         for (int offsetX = -1; offsetX < 2; offsetX++) {
+            //check that the vertical adjacent squares are in-bound
             if (posX + offsetX >= 0 && posX + offsetX < SIZE) {
                 for (int offsetY = -1; offsetY < 2; offsetY++) {
+                    //skip if looking at the same square
                     if (offsetX == 0 && offsetY == 0)
                         continue;
 
+                    //check that the horizontal adjacent squares are in-bound
                     if (posY + offsetY >= 0 && posY + offsetY < SIZE) {
+                        //check if the adjacent square is of different colour
                         if (checkerboard[posX + offsetX][posY + offsetY] == -colourTurn) {
+                            //if in that direction there is an allowed line then swap all the disks between the two extremes
                             if (checkDirection(offsetX, offsetY, posX + offsetX, posY + offsetY, colourTurn)) {
                                 int xTemp = posX;
                                 int yTemp = posY;
@@ -118,6 +143,7 @@ public class Checkerboard {
         }
     }
 
+    //return true if there is at least one possible move for the player, return false otherwise
     public boolean existAllowedPlace(int colourTurn) {
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
@@ -129,6 +155,7 @@ public class Checkerboard {
         return false;
     }
 
+    //refresh the counter of the placed disks of each color
     public void disksCount() {
 
         numberOfBlacks = 0;
@@ -143,9 +170,9 @@ public class Checkerboard {
                 }
             }
         }
-
     }
 
+    //marks all the allowed squares for a disk color on the checkerboard
     protected void addAllowedDisks(int colourTurn) {
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
@@ -154,9 +181,9 @@ public class Checkerboard {
                 }
             }
         }
-
     }
 
+    //removes the marking of all the allowed squares
     protected void removeAllowedDisks() {
         for (int i = 0; i < SIZE; i++)
             for (int j = 0; j < SIZE; j++)
@@ -164,7 +191,7 @@ public class Checkerboard {
 
     }
 
-
+    //return true if neither players have available placings and false otherwise
     public boolean gameOver() {
         if (!existAllowedPlace(B) && !existAllowedPlace(W)) {
             return true;
